5a0e8a42b173963d57d19293b7002312665c3a29
Man-V-hub <manuelignacio.sanhueza@alumnos.ulagos.cl>
2024-11-08 23:51:53 -0300

    Update App.js

--- REACT NATIVE/AppIntegracion/App.js
+++ REACT NATIVE/AppIntegracion/App.js
@@ -1,20 +1,89 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert, TouchableHighlight, Pressable } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Ionicons } from '@expo/vector-icons'; // Asegúrate de tener @expo/vector-icons instalado
 
 export default function App() {
+  const showAlert1 = () => {
+    Alert.alert('Alerta', 'Este es el mensaje del primer botón.');
+  };
+
+  const showAlert2 = () => {
+    Alert.alert('Alerta Personalizada', 'Este es el mensaje del segundo botón.');
+  };
+
+  const showAlert3 = () => {
+    Alert.alert('Alerta con Icono', 'Este es el mensaje del tercer botón.');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <LinearGradient
+      colors={['rgba(131,58,180,1)', 'rgba(253,29,29,1)', 'rgba(252,176,69,1)']}
+      style={styles.container}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 0, y: 1 }}
+    >
+      <View style={styles.imageContainer}>
+        <Image source={require('./assets/LOGO ORIGINAL TRANSPARENCIA.png')} style={styles.logo} />
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.customText}>Primer mensaje en una app y con un fondo degradado.</Text>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <Button title="PULSA AQUI" onPress={showAlert1} />
+        <TouchableHighlight style={styles.customButton} onPress={showAlert2}>
+          <Text style={styles.buttonText}>PRESIONA AQUI</Text>
+        </TouchableHighlight>
+        <Pressable style={styles.pressableButton} onPress={showAlert3}>
+          <Ionicons name="logo-google" size={24} color="white" />
+          <Text style={styles.buttonText}>ENVIAR</Text>
+        </Pressable>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageContainer: {
+    marginBottom: 20,
+  },
+  textContainer: {
+    marginBottom: 20,
+  },
+  buttonsContainer: {
+    alignItems: 'center',
+  },
+  customText: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: 'green',
+  },
+  logo: {
+    width: 100,
+    height: 110,
+  },
+  customButton: {
+    backgroundColor: '#2196F3',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  pressableButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#34A853',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: 'white',
+    marginLeft: 10,
   },
 });
