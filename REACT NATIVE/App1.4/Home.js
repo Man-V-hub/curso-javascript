57e065a31df0dfed65e2281627f4750d02d4a219
Man-V-hub <manuelignacio.sanhueza@alumnos.ulagos.cl>
2024-11-08 23:25:09 -0300

    Create Home.js

---
+++ REACT NATIVE/App1.4/Home.js
@@ -0,0 +1,23 @@
+// Home.js
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcomeText}>¡Bienvenido/a al Home!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  welcomeText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
