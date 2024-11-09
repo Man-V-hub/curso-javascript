9635a474cdefb78cb1bcb287c9cade5a0b5b90cf
Man-V-hub <manuelignacio.sanhueza@alumnos.ulagos.cl>
2024-11-08 22:14:58 -0300

    Update App.js

--- REACT NATIVE/App1.2/App.js
+++ REACT NATIVE/App1.2/App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <LinearGradient
+      colors={['rgba(131,58,180,1)', 'rgba(253,29,29,1)', 'rgba(252,176,69,1)']}
+      style={styles.container}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 0, y: 1 }}
+    >
+      <Image source={require('./assets/LOGO ORIGINAL TRANSPARENCIA.png')} style={styles.logo} />
+      <Text style={styles.customText}>Primer mensaje en una app y con un fondo degradado.</Text>
       <StatusBar style="auto" />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  customText: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: 'green',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
   },
 });
