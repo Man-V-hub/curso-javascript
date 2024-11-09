77938b45a2b7eddde88801d0762732d693dd4737
Man-V-hub <manuelignacio.sanhueza@alumnos.ulagos.cl>
2024-11-08 23:30:33 -0300

    Update App.js

--- REACT NATIVE/App1.4/App.js
+++ REACT NATIVE/App1.4/App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button, Alert, TouchableHighlight, Pressable } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import { Ionicons } from '@expo/vector-icons'; // Asegúrate de tener @expo/vector-icons instalado
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './Home'; // Importa la nueva pantalla
@@ -9,18 +8,6 @@ import Home from './Home'; // Importa la nueva pantalla
 const Stack = createNativeStackNavigator();
 
 function HomeScreen({ navigation }) {
-  const showAlert1 = () => {
-    Alert.alert('Alerta', 'Este es el mensaje del primer botón.');
-  };
-
-  const showAlert2 = () => {
-    Alert.alert('Alerta Personalizada', 'Este es el mensaje del segundo botón.');
-  };
-
-  const showAlert3 = () => {
-    Alert.alert('Alerta con Icono', 'Este es el mensaje del tercer botón.');
-  };
-
   return (
     <LinearGradient
       colors={['rgba(131,58,180,1)', 'rgba(253,29,29,1)', 'rgba(252,176,69,1)']}
@@ -35,15 +22,10 @@ function HomeScreen({ navigation }) {
         <Text style={styles.customText}>Primer mensaje en una app y con un fondo degradado.</Text>
       </View>
       <View style={styles.buttonsContainer}>
-        <Button title="PULSA AQUI" onPress={showAlert1} />
-        <TouchableHighlight style={styles.customButton} onPress={showAlert2}>
-          <Text style={styles.buttonText}>PRESIONA AQUI</Text>
-        </TouchableHighlight>
-        <Pressable style={styles.pressableButton} onPress={showAlert3}>
-          <Ionicons name="logo-google" size={24} color="white" />
-          <Text style={styles.buttonText}>ENVIAR</Text>
-        </Pressable>
-        <Button title="Ir al Home" onPress={() => navigation.navigate('Home')} />
+        <Button
+          title="Ir al Home"
+          onPress={() => navigation.navigate('Home')}
+        />
       </View>
       <StatusBar style="auto" />
     </LinearGradient>
@@ -84,23 +66,5 @@ const styles = StyleSheet.create({
   logo: {
     width: 100,
     height: 110,
-  },
-  customButton: {
-    backgroundColor: '#2196F3',
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 10,
-  },
-  pressableButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#34A853',
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 10,
-  },
-  buttonText: {
-    color: 'white',
-    marginLeft: 10,
   },
 });
